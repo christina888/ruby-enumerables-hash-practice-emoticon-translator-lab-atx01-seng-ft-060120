4ad8e0c7b7555b7777991b197d0d6f5ee446fe00
Christina Chianelli <cechianelli@gmail.com>
2020-05-29 16:41:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,24 @@
 # require modules here
+require "yaml"
+require 'pry'
+def load_library(file_path)
+  # code goes here
+  emoji_hash = {}
+  emoji = YAML.load_file(file_path)
+  emoji.each do |key, value|
+    emoji_hash[]
+    
+    
+ binding.pry
+end
+emoji_hash
+end
 
-def load_library
+def get_japanese_emoticon(file_path, emoji)
   # code goes here
 end
 
-def get_japanese_emoticon
+def get_english_meaning(file_path, emoji)
   # code goes here
-end
-
-def get_english_meaning
-  # code goes here
+  
 end
